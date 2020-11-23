63cc4c2d7db6d61a62ef6c57efc2a84678c9947b
phil <pri@irbe.ch>
2020-11-23 10:14:26 +0000

    header middleware allows $ServerIP for hosts ip

--- config/middlewares.go
+++ config/middlewares.go
@@ -1,5 +1,12 @@
 package config
 
+import (
+	"fmt"
+
+	"github.com/pheelee/traefik-admin/helpers"
+	"github.com/pheelee/traefik-admin/logger"
+)
+
 // Middleware defines Traefik Middleware
 type Middleware struct {
 	RedirectScheme RedirectScheme `yaml:"redirectScheme,omitempty"`
@@ -21,7 +28,19 @@ func (h *Headers) fromInput(c UserInput) {
 	h.CustomRequestHeaders = make(map[string]string)
 	for _, uh := range c.Headers {
 		if uh.Name != "" {
-			h.CustomRequestHeaders[uh.Name] = uh.Value
+			switch uh.Value {
+			case "$ServerIP":
+				ip, err := helpers.GetHostIP()
+				if err != nil {
+					logger.Warning(fmt.Sprintf("could not get ip address requested for header %s", uh.Name))
+					h.CustomRequestHeaders[uh.Name] = "n/a"
+				} else {
+					h.CustomRequestHeaders[uh.Name] = ip
+				}
+
+			case "":
+				h.CustomRequestHeaders[uh.Name] = uh.Value
+			}
 		}
 	}
 }
