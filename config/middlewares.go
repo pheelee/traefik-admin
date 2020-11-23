52e7b6be111561f15d35a8330c8375cb414d2b16
phil <pri@irbe.ch>
2020-11-23 09:50:57 +0000

    add headers middleware

--- config/middlewares.go
+++ config/middlewares.go
@@ -2,11 +2,26 @@ package config
 
 // Middleware defines Traefik Middleware
 type Middleware struct {
-	RedirectScheme RedirectScheme `yaml:"redirectScheme"`
+	RedirectScheme RedirectScheme `yaml:"redirectScheme,omitempty"`
+	Headers        Headers        `yaml:"headers,omitempty"`
 }
 
 // RedirectScheme holds data for a schema redirect
 type RedirectScheme struct {
 	Scheme    string `yaml:"scheme"`
 	Permanent bool   `yaml:"permanent"`
+}
+
+// Headers hold custom headers structure
+type Headers struct {
+	CustomRequestHeaders map[string]string `yaml:"customRequestHeaders,omitempty"`
+}
+
+func (h *Headers) fromInput(c UserInput) {
+	h.CustomRequestHeaders = make(map[string]string)
+	for _, uh := range c.Headers {
+		if uh.Name != "" {
+			h.CustomRequestHeaders[uh.Name] = uh.Value
+		}
+	}
 }
